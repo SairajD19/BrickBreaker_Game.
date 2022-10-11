bdac13605a78ba071a73e65c2f39784275c1c1fd
SairajD19 <115550127+SairajD19@users.noreply.github.com>
2022-10-11 17:25:59 +0530

    Add files via upload

---
+++ Demogame/Main.java
@@ -0,0 +1,17 @@
+package Demogame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+
+        obj.setBounds(10,10, 700, 600);
+        obj.setTitle("Brick Breaker Game");
+        obj.setVisible(true);
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
